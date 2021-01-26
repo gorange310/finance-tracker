dcb70b49403deb8cdb4a0809048967f8145dc979
Gene <gorange310@gmail.com>
2021-01-26 23:08:42 +0800

    update credentials file

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,10 @@
 class Stock < ApplicationRecord
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
-              publishable_token: 'Tpk_4bfb750a11fe4d6fa4e6a1474b03acf3',
+              publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
               endpoint: 'https://sandbox.iexapis.com/v1')
     client.price(ticker_symbol)
   end
-
 
 end
