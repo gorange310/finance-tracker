680180b2b2e9d39ecd104f0b86c1ce107f8d934e
Gene <gorange310@gmail.com>
2021-01-26 22:28:09 +0800

    Stock method: new_lookup

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+              publishable_token: 'Tpk_4bfb750a11fe4d6fa4e6a1474b03acf3',
+              endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
+
+
 end
