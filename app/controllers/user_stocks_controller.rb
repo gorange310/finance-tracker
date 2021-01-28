130467b698620c0c8c32248a11070577626d295a
Gene <gorange310@gmail.com>
2021-01-28 16:47:03 +0800

    remove tracking stock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -5,10 +5,18 @@ class UserStocksController < ApplicationController
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
       stock.save
-      @user_stock = UserStock.create(user: current_user, stock: stock)
-      flash[:notice] = "#{stock.name} was successfully added to stocks portfolio"
-      redirect_to my_portfolio_path
     end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash.now[:notice] = "#{stock.name} was successfully added to stocks portfolio"
+    redirect_to my_portfolio_path
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.find_by(stock_id: stock.id)
+    user_stock.destroy
+    flash.now[:notice] = "#{user_stock.stock.name} was successfully removed"
+    redirect_to my_portfolio_path
   end
 
 end
