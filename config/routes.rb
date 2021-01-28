1e1aa739017b326d16ad1755d4c1da74c8c440b6
Gene <gorange310@gmail.com>
2021-01-28 20:44:10 +0800

    show following list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   devise_for :users
   root "pages#home"
-  get "my_portfolio", to: "users#my_portfolio"
   get "search_stock", to: "stocks#search"
+  get "my_portfolio", to: "users#my_portfolio"
+  get "following", to: "users#following"
 end
