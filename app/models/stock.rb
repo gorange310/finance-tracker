0b6c85e482ebf14bba8ec01100856a8e3d2515fb
Gene <gorange310@gmail.com>
2021-02-01 17:22:11 +0800

    stock search: lowcase, upcase

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,13 +3,16 @@ class Stock < ApplicationRecord
   has_many :users, through: :user_stocks
 
   validates :name, :ticker, presence: true
+  validates_uniqueness_of :ticker
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
               publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
               endpoint: 'https://sandbox.iexapis.com/v1')
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: ticker_symbol, 
+          name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
     rescue => exception
       nil
     end
@@ -18,4 +21,6 @@ class Stock < ApplicationRecord
   def self.check_db(ticker_symbol)
     find_by(ticker: ticker_symbol)
   end
+
+
 end
