a0b7dcf68f8778ab0df4434309d895069492d757
Gene <gorange310@gmail.com>
2021-02-01 23:51:19 +0800

    time.zone, update when search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,19 +2,23 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker, presence: true
-  validates_uniqueness_of :ticker
+  validates :name, :ticker, presence: true, uniqueness: {case_sensitive: false}
+
+  before_create { self.ticker = ticker.upcase }
+  
+  
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
               publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
               endpoint: 'https://sandbox.iexapis.com/v1')
-    begin
+    stock = Stock.check_db(ticker_symbol.upcase!)
+    if stock
+      update_price(stock.id)
+    else
       new(ticker: ticker_symbol, 
           name: client.company(ticker_symbol).company_name,
           last_price: client.price(ticker_symbol))
-    rescue => exception
-      nil
     end
   end
 
@@ -28,6 +32,5 @@ class Stock < ApplicationRecord
               endpoint: 'https://sandbox.iexapis.com/v1')
     Stock.update(stock_id, last_price: client.price(Stock.find(stock_id).ticker))
   end
-
 
 end
