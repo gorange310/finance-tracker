3c5067d19e2a940bc99c5026dee15cbc02783430
Gene <gorange310@gmail.com>
2021-03-03 18:38:12 +0800

    debug: new_lookup upcase

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,8 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker, presence: true, uniqueness: {case_sensitive: false}
+  validates :name
+  validates :ticker, presence: true, uniqueness: {case_sensitive: false}
 
   before_create { self.ticker = ticker.upcase }
 
@@ -11,10 +12,13 @@ class Stock < ApplicationRecord
               publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
               endpoint: 'https://sandbox.iexapis.com/v1')
     begin client.quote(ticker_symbol)
-      stock = Stock.check_db(ticker_symbol.upcase!)
+      stock = Stock.check_db(ticker_symbol.upcase)
+      puts stock.inspect
       if stock
+        puts "if"
         update_price(stock.id)
       else
+        puts "else"
         create!(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
       end
     rescue => exception
@@ -23,6 +27,8 @@ class Stock < ApplicationRecord
   end
 
   def self.check_db(ticker_symbol)
+    puts "check db +++++"
+    puts ticker_symbol
     find_by(ticker: ticker_symbol)
   end
 
