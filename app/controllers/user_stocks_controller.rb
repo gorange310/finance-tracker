4e25b68146b7d98010ebe14f076bc9573694c37b
Gene <gorange310@gmail.com>
2021-03-03 21:15:32 +0800

    update UserStocks create method

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,11 +1,11 @@
 class UserStocksController < ApplicationController
 
   def create
-    stock = Stock.check_db(params[:ticker])
-    if stock.blank?
-      stock = Stock.new_lookup(params[:ticker])
-      stock.save
-    end
+    stock = Stock.new_lookup(params[:ticker])
+    # if stock.blank?
+    #   stock = Stock.new_lookup(params[:ticker])
+    #   stock.save
+    # end
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash.now[:notice] = "#{stock.name} was successfully added to stocks portfolio"
     redirect_to my_portfolio_path
