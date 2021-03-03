dba738cc8864ab5f778a70f54bd61c5fb85fea46
Gene <gorange310@gmail.com>
2021-03-03 21:20:35 +0800

    update Stock validatation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,9 +2,12 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, :ticker,
+            presence: true,
+            uniqueness: {case_sensitive: false},
+            :on => :create
 
-  before_create { self.ticker = ticker.upcase }
+  before_save { self.ticker = ticker.upcase }
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
